8642872fa585319be69544314221745b851fb47a
OskarDubas-dev <oohdee.dev@gmail.com>
2022-11-12 08:00:57 +0000

    get todos function

--- App1_ToDoList/main.py
+++ App1_ToDoList/main.py
@@ -3,6 +3,21 @@ todos = []
 
 # change while exit to try/except
 
+
+FILEPATH = "todos_item.txt"
+
+def get_todos(filepath=FILEPATH):
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+
+
+
+
+
+
 while True:
     status = input(user_prompt)
     status = status.strip()
