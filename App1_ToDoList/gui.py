46374d04b2e29145ca3cc1a5f4142458489ba211
OskarDubas-dev <oohdee.dev@gmail.com>
2022-11-16 10:19:11 +0000

    gui imported and window created

---
+++ App1_ToDoList/gui.py
@@ -0,0 +1,21 @@
+import PySimpleGUI as sg
+
+add_button = sg.Button("Add")
+quit_button = sg.Button("Quit")
+
+layout = \
+    [
+        [sg.Text("ToDo:",key="-OUTPUT-"), sg.InputText(tooltip="Enter ToDo", key="-INPUT-"), add_button],
+        [quit_button]
+    ]
+
+# create window
+window = sg.Window("ToDo App", layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED or event == "Quit":
+        break
+    window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying PySimpleGUI")
+
+window.close()
