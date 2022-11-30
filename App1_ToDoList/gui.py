6914be249e1a97eb66e867119d801b0631d05b68
OskarDubas-dev <oohdee.dev@gmail.com>
2022-11-30 18:38:57 +0000

    fix get_path

--- App1_ToDoList/gui.py
+++ App1_ToDoList/gui.py
@@ -17,6 +17,7 @@ def resource_path(relative_path):
 
 
 def get_path(filename):
+    """ Use this when icons are inside a folder """
     if hasattr(sys, "_MEIPASS"):
         return os.path.join(sys._MEIPASS, filename)
     else:
