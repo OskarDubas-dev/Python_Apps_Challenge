a991c37adbbe37c48e7a410ab33a54b6ca7801d1
OskarDubas-dev <oohdee.dev@gmail.com>
2022-12-01 11:07:50 +0000

    implement add todos to list

--- App1_ToDoList/web.py
+++ App1_ToDoList/web.py
@@ -4,12 +4,17 @@ import functions
 todos = functions.get_todos()
 label = "Enter a Todo:"
 
-st.title("My ToDo App")
 
+def add_todo():
+    _todo = st.session_state["new_todo"] + "\n"
+    todos.append(_todo)
+    functions.write_todos(todos)
+    st.session_state["new_todo"] = ""
+
+
+st.title("My ToDo App")
 
 for todo in todos:
     st.checkbox(todo)
 
-
-
-st.text_input(label="", placeholder=label)
+st.text_input(key="new_todo", label="", placeholder=label, on_change=add_todo)
