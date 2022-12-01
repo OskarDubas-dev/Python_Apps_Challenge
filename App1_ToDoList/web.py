0d43d551c0226edcba2b86f8807aec2895133677
OskarDubas-dev <oohdee.dev@gmail.com>
2022-12-01 10:29:33 +0000

    building webapp, 1st step getting todos and display

--- App1_ToDoList/web.py
+++ App1_ToDoList/web.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import functions
+
+todos = functions.get_todos()
+label = "Enter a Todo:"
 
 st.title("My ToDo App")
+
+
+for todo in todos:
+    st.checkbox(todo)
+
+
+
+st.text_input(label="", placeholder=label)
