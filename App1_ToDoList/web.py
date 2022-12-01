ad27d0fd7185c051a5a6b082637768aea2ee716a
OskarDubas-dev <oohdee.dev@gmail.com>
2022-12-01 17:58:48 +0000

    implement "delete items from todo list"

--- App1_ToDoList/web.py
+++ App1_ToDoList/web.py
@@ -14,7 +14,16 @@ def add_todo():
 
 st.title("My ToDo App")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key="checkbox_" + todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state["checkbox_"+todo]
+        st._rerun()
+
 
 st.text_input(key="new_todo", label="", placeholder=label, on_change=add_todo)
+
+
+st.session_state
