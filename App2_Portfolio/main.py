d50677327e6d02f38c896670228ead246f3b086c
OskarDubas-dev <oohdee.dev@gmail.com>
2023-01-10 09:33:10 +0000

    fix typo

--- App2_Portfolio/main.py
+++ App2_Portfolio/main.py
@@ -5,13 +5,13 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.jpg")
 
 with col2:
     st.title("Oskar Dubas")
     content = """
     Hello! I am Oskar. I am a programmer and a designer. My preferred programming languages are C++, Python, C# and JavaScript.
-    I also like to work with game engines, specifically Unreal Engine, Unity and Godot. Video Games design is my passion but I also enjoy Web and Software Development.
+    I like working with game engines, specifically Unreal Engine, Unity and Godot. Video Game design is my passion but I also enjoy Web and Software Development.
     """
 
     st.write(content)
