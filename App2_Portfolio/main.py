2ac5bc40582be676c30562c580ca0f8c5308995b
OskarDubas-dev <oohdee.dev@gmail.com>
2023-01-10 11:12:00 +0000

    add images to website and links

--- App2_Portfolio/main.py
+++ App2_Portfolio/main.py
@@ -2,6 +2,12 @@ import streamlit as st
 import pandas
 
 st.set_page_config(layout="wide")
+persian_green = "#00a7a2"
+piction_blue = "#46cae4"
+ship_gray = "#3a393f"
+sulu = "#abed5d"
+
+
 
 col1, col2 = st.columns(2)
 
@@ -17,14 +23,20 @@ with col2:
 
     st.write(content)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
